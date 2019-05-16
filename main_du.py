a154dc68b5fbe7c940e86b2848ef1057098efe89
forgi86 <marco.forgione1986@gmail.com>
2019-05-16 17:32:11 +0200

    Working on the du file. Still to finish

--- main_du.py
+++ main_du.py
@@ -4,50 +4,87 @@ import scipy as sp
 import scipy.sparse as sparse
 import time
 
-# Discrete time model of a quadcopter
-Ts = 0.2
-M = 2
+# Discrete time model of the system (mass point with input force and friction)
+
+# Constants #
+Ts = 0.2 # sampling time (s)
+M = 2    # mass (Kg)
+b = 0.3  # friction coefficient (N*s/m)
+
 Ad = sparse.csc_matrix([
     [1.0, Ts],
-    [0,  1.0]
+    [0,  1.0 -b/M*Ts]
 ])
 Bd = sparse.csc_matrix([
   [0.0],
   [Ts/M]])
 
+# Continous-time matrices (just for reference)
+Ac = np.array([
+    [0.0, 1.0],
+    [0, -b/M]]
+)
+Bc = np.array([
+    [0.0],
+    [1/M]
+])
+
 [nx, nu] = Bd.shape # number of states and number or inputs
 
-# Constraints
-uref = 0
-uinit = 0 # not used here
-umin = np.array([-1000.0]) - uref
-umax = np.array([1000.0]) - uref
-
-xmin = np.array([-100.0, -100.0])
-xmax = np.array([100.0,   100.0])
-
-# Objective function
-Q = sparse.diags([0.2, 0.3])
-QN = sparse.diags([0.4, 0.5]) # final cost
-R = 0.1*sparse.eye(1)
-
-# Initial and reference states
-x0 = np.array([0.1, 0.2]) # initial state
 # Reference input and states
 pref = 7.0
 vref = 0
 xref = np.array([pref, vref]) # reference state
+uref = 0      # reference input
+uinit =  np.array([0.0])  # input at time step negative one - used to penalize the first delta 0. Could be the same as uref.
+
+# Constraints
+xmin = np.array([-100.0, -100.0])
+xmax = np.array([100.0,   100.0])
+
+umin = np.array([-1.5])
+umax = np.array([1.5])
+
+Dumin = np.array([-2e-1])
+Dumax = np.array([2e-1])
+
+# Objective function
+Qx = sparse.diags([0.5, 0.1])   # Quadratic cost for states x0, x1, ..., x_N-1
+QxN = sparse.diags([0.5, 0.1])  # Quadratic cost for xN
+Qu = 0.0 * sparse.eye(1)        # Quadratic cost for u0, u1, ...., u_N-1
+QDu = 2.0 * sparse.eye(1)       # Quadratic cost for Du0, Du1, ...., Du_N-1
+
+# Initial state
+x0 = np.array([0.1, 0.2]) # initial state
 
 # Prediction horizon
 Np = 20
 
 # Cast MPC problem to a QP: x = (x(0),x(1),...,x(N),u(0),...,u(N-1))
 # - quadratic objective
-P = sparse.block_diag([sparse.kron(sparse.eye(Np), Q), QN,
-                       sparse.kron(sparse.eye(Np), R)]).tocsc()
+P = sparse.block_diag([sparse.kron(sparse.eye(Np), Qx),     # x0... x_N-1
+                       QxN,                                 # x_N
+                       sparse.kron(sparse.eye(Np), Qu)]     # u0... u_N-1
+                      ).tocsc()
+
+iDu = 2 * np.eye(Np) + np.eye(Np,k=1) + np.eye(Np, k=-1)
+iDu[Np-1,Np-1] = 1
+P_dU = sparse.block_diag([sparse.kron(sparse.eye((Np+1)*nx), 0), # zeros for x
+                          sparse.kron(iDu, QDu)]
+                         ).tocsc()
+
+P = P + P_dU
+
 # - linear objective
-q = np.hstack([np.kron(np.ones(Np), -Q.dot(xref)), -QN.dot(xref),
-               np.zeros(Np * nu)])
+q = np.hstack([np.kron(np.ones(Np), -Qx.dot(xref)),         # x0... x_N-1
+               -QxN.dot(xref),                              # x_N
+               np.zeros(Np * nu)])                          # u0... u_N-1
+
+if not np.any(np.isnan(uinit)):
+    q_dU = np.hstack([np.zeros((Np+1) * nx),                # x0... x_N
+                   -QDu.dot(uinit),                         # u0
+                   np.zeros((Np-1) * nu)])                  # u0... u_N-1
+    q = q + q_dU
 
 # - linear dynamics
 Ax = sparse.kron(sparse.eye(Np + 1), -sparse.eye(nx)) + sparse.kron(sparse.eye(Np + 1, k=-1), Ad)
@@ -59,6 +96,7 @@ ueq = leq # for equality constraints -> upper bound  = lower bound!
 Aineq = sparse.eye((Np + 1) * nx + Np * nu)
 lineq = np.hstack([np.kron(np.ones(Np + 1), xmin), np.kron(np.ones(Np), umin)]) # lower bound of inequalities
 uineq = np.hstack([np.kron(np.ones(Np + 1), xmax), np.kron(np.ones(Np), umax)]) # upper bound of inequalities
+
 # - OSQP constraints
 A = sparse.vstack([Aeq, Aineq]).tocsc()
 l = np.hstack([leq, lineq])
