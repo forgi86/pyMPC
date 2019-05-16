601b17982535f3a35f06ddd642fdca2e625f3696
forgi86 <marco.forgione1986@gmail.com>
2019-05-16 13:25:27 +0200

    Improved the old example files. CVXpy and quadratic solver match perfectly!

--- main_cvxpy_simple.py
+++ main_cvxpy_simple.py
@@ -2,9 +2,9 @@ from cvxpy import *
 import numpy as np
 import scipy as sp
 import scipy.sparse as sparse
-
+import time
 # Discrete time model of a quadcopter
-Ts = 0.1
+Ts = 0.2
 M = 2
 Ad = sparse.csc_matrix([
     [1.0, Ts],
@@ -18,6 +18,7 @@ Bd = sparse.csc_matrix([
 
 # Constraints
 uref = 0
+uinit = 0 # not used here
 umin = np.array([-1000.0]) - uref
 umax = np.array([1000.0]) - uref
 
@@ -31,10 +32,13 @@ R = 0.1*sparse.eye(1)
 
 # Initial and reference states
 x0 = np.array([0.1, 0.2]) # initial state
-xr = np.array([50.0, 50.0]) # reference state
+# Reference input and states
+pref = 7.0
+vref = 0
+xref = np.array([pref, vref]) # reference state
 
 # Prediction horizon
-Np = 10
+Np = 20
 
 
 # Define problem
@@ -44,18 +48,52 @@ x_init = Parameter(nx)
 objective = 0
 constraints = [x[:,0] == x_init]
 for k in range(Np):
-    objective += quad_form(x[:,k] - xr, Q) + quad_form(u[:,k], R)
+    objective += quad_form(x[:,k] - xref, Q) + quad_form(u[:, k], R)
     constraints += [x[:,k+1] == Ad*x[:,k] + Bd*u[:,k]]
     constraints += [xmin <= x[:,k], x[:,k] <= xmax]
     constraints += [umin <= u[:,k], u[:,k] <= umax]
-objective += quad_form(x[:, Np] - xr, QN)
+objective += quad_form(x[:, Np] - xref, QN)
 prob = Problem(Minimize(objective), constraints)
 
+
 # Simulate in closed loop
-nsim = 100
+# Simulate in closed loop
+len_sim = 15 # simulation length (s)
+nsim = int(len_sim/Ts) # simulation length(timesteps)
+xsim = np.zeros((nsim,nx))
+usim = np.zeros((nsim,nu))
+tsim = np.arange(0,nsim)*Ts
+
+uminus1_val = uinit # initial previous measured input is the input at time instant -1.
+time_start = time.time()
 for i in range(nsim):
     x_init.value = x0
+    #uminus1.value = uminus1_val
     prob.solve(solver=OSQP, warm_start=True)
-    x0 = Ad.dot(x0) + Bd.dot(u[:,0].value)
-    u0 = u[:,0].value
+    uMPC = u[:,0].value
+    usim[i,:] = uMPC
+    x0 = Ad.dot(x0) + Bd.dot(uMPC)
+    xsim[i,:] = x0
 
+    uminus1_val = uMPC # or a measurement if the input is affected by noise
+time_sim = time.time() - time_start
+
+# In [1]
+import matplotlib.pyplot as plt
+fig,axes = plt.subplots(3,1, figsize=(10,10))
+axes[0].plot(tsim, xsim[:,0], "k", label='p')
+axes[0].plot(tsim, xref[0]*np.ones(np.shape(tsim)), "r--", label="pref")
+axes[0].set_title("Position (m)")
+
+axes[1].plot(tsim, xsim[:,1], label="v")
+axes[1].plot(tsim, xref[1]*np.ones(np.shape(tsim)), "r--", label="vref")
+axes[1].set_title("Velocity (m/s)")
+
+axes[2].plot(tsim, usim[:,0], label="u")
+axes[2].plot(tsim, uref*np.ones(np.shape(tsim)), "r--", label="uref")
+axes[2].set_title("Force (N)")
+
+
+for ax in axes:
+    ax.grid(True)
+    ax.legend()
--- main_simple.py
+++ main_simple.py
@@ -2,9 +2,10 @@ import osqp
 import numpy as np
 import scipy as sp
 import scipy.sparse as sparse
+import time
 
 # Discrete time model of a quadcopter
-Ts = 0.1
+Ts = 0.2
 M = 2
 Ad = sparse.csc_matrix([
     [1.0, Ts],
@@ -18,6 +19,7 @@ Bd = sparse.csc_matrix([
 
 # Constraints
 uref = 0
+uinit = 0 # not used here
 umin = np.array([-1000.0]) - uref
 umax = np.array([1000.0]) - uref
 
@@ -31,17 +33,20 @@ R = 0.1*sparse.eye(1)
 
 # Initial and reference states
 x0 = np.array([0.1, 0.2]) # initial state
-xr = np.array([50.0, 50.0]) # reference state
+# Reference input and states
+pref = 7.0
+vref = 0
+xref = np.array([pref, vref]) # reference state
 
 # Prediction horizon
-Np = 10
+Np = 20
 
 # Cast MPC problem to a QP: x = (x(0),x(1),...,x(N),u(0),...,u(N-1))
 # - quadratic objective
 P = sparse.block_diag([sparse.kron(sparse.eye(Np), Q), QN,
                        sparse.kron(sparse.eye(Np), R)]).tocsc()
 # - linear objective
-q = np.hstack([np.kron(np.ones(Np), -Q.dot(xr)), -QN.dot(xr),
+q = np.hstack([np.kron(np.ones(Np), -Q.dot(xref)), -QN.dot(xref),
                np.zeros(Np * nu)])
 
 # - linear dynamics
@@ -66,7 +71,14 @@ prob = osqp.OSQP()
 prob.setup(P, q, A, l, u, warm_start=True)
 
 # Simulate in closed loop
-nsim = 1
+len_sim = 15 # simulation length (s)
+nsim = int(len_sim/Ts) # simulation length(timesteps)
+xsim = np.zeros((nsim,nx))
+usim = np.zeros((nsim,nu))
+tsim = np.arange(0,nsim)*Ts
+
+#uminus1_val = uinit # initial previous measured input is the input at time instant -1.
+time_start = time.time()
 for i in range(nsim):
     # Solve
     res = prob.solve()
@@ -75,14 +87,34 @@ for i in range(nsim):
     if res.info.status != 'solved':
         raise ValueError('OSQP did not solve the problem!')
 
-    x0 = res.x[0:nx]
-    x1 = res.x[nx:2*nx]
-    u0 = res.x[-Np * nu:-(Np - 1) * nu]
     # Apply first control input to the plant
-    #ctrl = res.x[-Np * nu:-(Np - 1) * nu]
-    #x0 = Ad.dot(x0) + Bd.dot(ctrl)
+    uMPC = res.x[-Np * nu:-(Np - 1) * nu]
+    x0 = Ad.dot(x0) + Bd.dot(uMPC)
+    xsim[i,:] = x0
+    usim[i,:] = uMPC
 
     # Update initial state
-    #l[:nx] = -x0
-    #u[:nx] = -x0
-    #prob.update(l=l, u=u)
+    l[:nx] = -x0
+    u[:nx] = -x0
+    prob.update(l=l, u=u)
+time_sim = time.time() - time_start
+
+# In [1]
+import matplotlib.pyplot as plt
+fig,axes = plt.subplots(3,1, figsize=(10,10))
+axes[0].plot(tsim, xsim[:,0], "k", label='p')
+axes[0].plot(tsim, xref[0]*np.ones(np.shape(tsim)), "r--", label="pref")
+axes[0].set_title("Position (m)")
+
+axes[1].plot(tsim, xsim[:,1], label="v")
+axes[1].plot(tsim, xref[1]*np.ones(np.shape(tsim)), "r--", label="vref")
+axes[1].set_title("Velocity (m/s)")
+
+axes[2].plot(tsim, usim[:,0], label="u")
+axes[2].plot(tsim, uref*np.ones(np.shape(tsim)), "r--", label="uref")
+axes[2].set_title("Force (N)")
+
+
+for ax in axes:
+    ax.grid(True)
+    ax.legend()
